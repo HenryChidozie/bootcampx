dd73fb088c8f4c7f07a24bcfb0b2da7cc0b8b70b
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-04 00:32:19 -0500

    Queries 3 GROUP BY files

--- xtras/join/3_left_outer_join.sql
+++ xtras/join/3_left_outer_join.sql
@@ -1,8 +1,6 @@
 
 /*
 All students and only cohorts that have students. students is the table to the left of the word LEFT.
-The LEFT OUTER JOIN will return all of the students, even ones without a cohort_id.
-
 */
 
 SELECT students.name as student_name, email, cohorts.name as cohort_name
--- xtras/join/4_right_outer_join.sql
+++ xtras/join/4_right_outer_join.sql
@@ -1,7 +1,5 @@
 /* 
 Only students that have a cohort_id and all cohorts. cohorts is the table to the right of the word RIGHT.
-The RIGHT OUTER JOIN will return all cohorts, even ones without any students enrolled.
-
 */
 
 SELECT students.name as student_name, email, cohorts.name as cohort_name
